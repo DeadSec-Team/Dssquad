861310b1aff4b33d962f3f31db82408b3ab80b77
DeadSec Team <50407545+DeadSec-Team@users.noreply.github.com>
2019-05-13 15:45:07 +0700

    F

---
+++ deadsec.sh
@@ -0,0 +1,116 @@
+clear
+toilet -f slant --gay "DSsquad"
+sleep 2
+echo 
+echo "\033[31;1mCreated By: DeadSec Team"
+sleep 1
+echo "\033[31;1mAuthor By : FirmansyahKen/XOX758/DarkNess"
+sleep 1
+echo "\033[31;1mGithub    : https://github.com/deadsec"
+sleep 1
+echo "\033[31;1mThanks To : All member DeadSec"
+sleep 1
+echo
+echo "\033[36;1m+ \033[31;1m===== \033[33;1mTools DeadSec 2019 v 0.1 \033[31;1m=====\033[36;1m +"
+echo
+echo "\033[31;1mSelect tools yang tersedia:"
+echo
+echo "\033[34;1m[1]. \033[36;1mTelkomsel Spam"
+sleep 0.04
+echo "\033[34;1m[2]. \033[36;1mFacebook Bruteforce"
+sleep 0.04
+echo "\033[34;1m[3]. \033[36;1mDdos Attack"
+sleep 0.04
+echo "\033[34;1m[4]. \033[36;1mApi Termux"
+sleep 0.04
+echo "\033[34;1m[5]. \033[36;1mAdmin Finder"
+sleep 0.04
+echo "\033[34;1m[6]. \033[36;1mTools Installer"
+sleep 0.04
+echo "\033[34;1m[7]. \033[36;1mSteganography"
+echo "\033[34;1m[8]. \033[36;1mSimpel Webdav"
+sleep 0.03
+echo
+echo "\033[34;1m[0]. \033[31;1mExit"
+sleep 0.05
+echo 
+echo "\033[37;1mselect nomer"
+read mrrm 
+
+if 
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+sleep 2
+echo "\033[31;1mTerima Kasih Sudah Install Tools Kami"
+sleep 2
+clear
+echo "\033[37;1m"
+cd
+fi
+
+if
+[ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant --gay "Telkomsel"
+sleep 1
+echo "\033[31;1mAuthor By: FirmansyahKen"
+sleep 2
+clear
+cd tool
+php Telkomsel.php
+fi
+
+if
+[ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+toilet -f slant --gay "Ddos"
+sleep 2
+echo "\033[31;1mAuthor By: XOX758"
+sleep 2
+clear
+cd tool
+python2 ddos.py
+fi
+
+if
+[ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f slant --gay "Termux-Api"
+sleep 2 
+echo "\033[31;1mAuthor By: FirmansyahKen"
+sleep 2
+clear
+cd tool
+sh api.sh
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant --gay "Steganography"
+sleep 2
+echo 
+echo "\033[31;1mAuthor By: DarkNess"
+sleep 2
+clear
+cd tool
+sh creatorstegano.sh
+fi
+
+if
+[ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+toilet -f slant --gay "Webdav"
+sleep 2
+echo 
+echo "\033[31;1mAuthor By: DarkNess"
+sleep 2
+clear
+cd tool
+sh webdav.sh
+fi
